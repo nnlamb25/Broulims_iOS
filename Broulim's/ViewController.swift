271c2b1e90c16967918cb538e4860af9b7a55772
nnlamb25 <nnlamb25@gmail.com>
2017-07-18 10:41:04 -0700

    Initial Commit

---
+++ Broulim's/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Broulim's
+//
+//  Created by Nathan Lamb on 7/18/17.
+//  Copyright © 2017 Bench Press Broulims. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
